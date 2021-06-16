5562d4a51d69497a8728dcbfddd0f7945fddec80
David Maier <maierdavid1996@gmail.com>
2021-06-16 18:19:29 +0200

    extract drawing functions

--- src/web/src/app/services/bubble-chart-builder.service.ts
+++ src/web/src/app/services/bubble-chart-builder.service.ts
@@ -26,37 +26,51 @@ export class BubbleChartBuilderService {
     width = 500 - margin.left - margin.right,
     height = 420 - margin.top - margin.bottom;
 
-      // Add X axis
-  var x = d3.scaleLinear()
-  .domain([0, 10000])
-  .range([ 0, width ]);
-root.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x));
+    var x = this.drawXAxis(width, root, height);
 
-// Add Y axis
-var y = d3.scaleLinear()
-  .domain([35, 90])
-  .range([ height, 0]);
-root.append("g")
-  .call(d3.axisLeft(y));
+    var y = this.drawYAxis(height, root);
 
-// Add a scale for bubble size
-var z = d3.scaleLinear()
-  .domain([200000, 1310000000])
-  .range([ 1, 40]);
+    var z = this.scaleLinear();
 
-// Add dots
-root.append('g')
-  .selectAll("dot")
-  .data(data)
-  .enter()
-  .append("circle")
-    .attr("cx", function (d: any) { return x(d.gdpPercap); } )
-    .attr("cy", function (d: any) { return y(d.lifeExp); } )
-    .attr("r", function (d: any) { return z(d.pop); } )
-    .style("fill", "#69b3a2")
-    .style("opacity", "0.7")
-    .attr("stroke", "black")
+    this.addDots(root, data, x, y, z);
+  }
+
+  private addDots(root: SvgSelection, data: any, x: d3.ScaleLinear<number, number, never>, y: d3.ScaleLinear<number, number, never>, z: d3.ScaleLinear<number, number, never>) {
+    root.append('g')
+      .selectAll("dot")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("cx", function (d: any) { return x(d.gdpPercap); })
+      .attr("cy", function (d: any) { return y(d.lifeExp); })
+      .attr("r", function (d: any) { return z(d.pop); })
+      .style("fill", "#69b3a2")
+      .style("opacity", "0.7")
+      .attr("stroke", "black");
+  }
+
+  private scaleLinear() {
+    return d3.scaleLinear()
+      .domain([200000, 1310000000])
+      .range([1, 40]);
+  }
+
+  private drawYAxis(height: number, root: SvgSelection) {
+    var y = d3.scaleLinear()
+      .domain([35, 90])
+      .range([height, 0]);
+    root.append("g")
+      .call(d3.axisLeft(y));
+    return y;
+  }
+
+  private drawXAxis(width: number, root: SvgSelection, height: number) {
+    var x = d3.scaleLinear()
+      .domain([0, 10000])
+      .range([0, width]);
+    root.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+    return x;
   }
 }
