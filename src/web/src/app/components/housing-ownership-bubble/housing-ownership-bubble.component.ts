1dbc1ee49af311255b420966856cdf726d19bf88
David Maier <maierdavid1996@gmail.com>
2021-06-20 17:16:00 +0200

    increase font size

--- src/web/src/app/components/housing-ownership-bubble/housing-ownership-bubble.component.ts
+++ src/web/src/app/components/housing-ownership-bubble/housing-ownership-bubble.component.ts
@@ -5,11 +5,6 @@ import {
   BubbleChartBuilderService,
   ChartManipulatorService,
   SvgSelection,
-  BarSelection,
-  VoronoiChartBuilderService,
-  VoronoiSelection,
-  CIRCULAR_BAR_CHART,
-  VORONOI_CHART
 } from 'src/app/services';
 
 const data = [
@@ -26,13 +21,12 @@ const data = [
 })
 
 export class HousingOwnershipBubbleComponent implements AfterViewInit {
-  // private readonly height = 1000;
-  // private readonly width = 1000;
-
   private svg;
-  private margin = 50;
-  private width = 750 - (this.margin * 2);
-  private height = 400 - (this.margin * 2);
+  private margin = 175;
+  private width = 1800 - (this.margin * 2);
+  private height = 1440 - (this.margin * 2);
+
+  private fontSize = 50;
 
   private svgRoot: SvgSelection;
 
@@ -79,18 +73,20 @@ private drawPlot(): void {
   .range([ 0, this.width ]);
   this.svg.append("g")
   .attr("transform", "translate(0," + this.height + ")")
-  .call(d3.axisBottom(x).tickFormat(d3.format("d")));
+  .call(d3.axisBottom(x).tickFormat(d => d + "%"))
+  .style("font-size", `${this.fontSize}px`);
 
   // Add Y axis
   const y = d3.scaleLinear()
   .domain([0, 100])
   .range([ this.height, 0]);
   this.svg.append("g")
-  .call(d3.axisLeft(y));
+  .call(d3.axisLeft(y).tickFormat(d => d + "%"))
+  .style("font-size", `${this.fontSize}px`);;
 
   const z = d3.scaleLinear()
   .domain([0, 10000])
-  .range([0,40 ]);
+  .range([0, 100]);
 
   // Add dots
   const dots = this.svg.append('g');
